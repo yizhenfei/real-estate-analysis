8c29f0e11951bd263896fbfe7b714adcfca39e73
Yi Zhenfei <yizhenfei@outlook.com>
2018-11-06 20:32:41 +0800

    Enlarge address field in community table to 512

--- create-community-table.sql
+++ create-community-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE community (
     id INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(128),
-    address VARCHAR(256),
+    address VARCHAR(512),
     year_built YEAR,
     building_num INT,
     home_num INT
